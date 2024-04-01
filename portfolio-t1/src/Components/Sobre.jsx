23d678812b51afa1a989c956fe4f8f3cce2bd3f2
karynnemoreira <karynnemoreira@yahoo.com.br>
2024-04-01 10:35:45 -0300

    responsivo

--- portfolio-t1/src/Components/Sobre.jsx
+++ portfolio-t1/src/Components/Sobre.jsx
@@ -1,11 +1,68 @@
 import React from "react"
 import * as S from "./Style.jsx"
+import Vnw from "./imagensPortfolio/vnw.png"
+import Css from "./imagensPortfolio/css.png"
+import Js from "./imagensPortfolio/js.png"
+import Html from "./imagensPortfolio/html.png"
+import Mic from "./imagensPortfolio/mic.png"
 
 function Sobre() {
     return (
-        <>
-                <h2> sou o sobre</h2>
-        </>
+        <S.MainSobre>
+
+            <S.SectionDoSobre>
+                <h2>ESTUDOS</h2>
+                <S.Card>
+                    <S.DivCard>
+                        <h2>Desenvolvimento front-end</h2>
+                        <h3>AGO/2023 - MAIO/2024</h3>
+                        <img src={Vnw} alt="Logo do vnw" />
+                    </S.DivCard>
+
+                </S.Card>
+
+            </S.SectionDoSobre>
+
+            <S.SectionDoSobre>
+                <h2>HABILIDADES</h2>
+                <S.Card>
+                    <S.DivCard2>
+                        <div>
+                          <h3>CSS</h3>
+                        <img src={Css} alt="logo css" />  
+                        </div>
+                        
+                        <div>
+                          <h3>Js</h3>
+                        <img src={Js} alt="logo javascript" />  
+                        </div>
+
+                        <div>
+                             <h3>HTML</h3>
+                        <img src={Html} alt="logo html5" />
+                        </div>
+                        
+                       
+
+                    </S.DivCard2>
+                </S.Card>
+
+            </S.SectionDoSobre>
+
+            <S.SectionDoSobre>
+                <h2>PROFISSIONAL</h2>
+                <S.Card>
+                    <S.DivCard>
+                        <h2> cantora, compositora, produtora musical e atriz</h2>
+                        <img src={Mic} alt="Logo do microfone" />
+                    </S.DivCard>
+
+                </S.Card>
+
+            </S.SectionDoSobre>
+
+        </S.MainSobre>
+
     )
 }
 
--- portfolio-t1/src/Components/Style.jsx
+++ portfolio-t1/src/Components/Style.jsx
@@ -3,18 +3,22 @@ import styled from "styled-components"
 //----Estilização da navegação
 
 export const Nav = styled.nav`
-border: solid red;
 
 `
 
 export const Lista = styled.ul`
 width: 100%;
-border: solid green;
 display: flex;
 justify-content: space-around;
 list-style: none;
 height: 20vh;
 align-items: center;
+@media (min-width: 320px) and (max-width: 768px){
+    display: flex; 
+    flex-direction: column;
+    height: 40vh;
+    border: solid red;
+}
 
 
 a{
@@ -32,6 +36,7 @@ export const Perfil = styled.img`
     width: 20vw;
     height: 40vh;
     border-radius: 50%;
+    
 `
 
 export const ContainerInicio = styled.main`
@@ -40,10 +45,18 @@ justify-content:space-evenly;
 align-items: center;
 padding-top: 3rem;
 
+@media (min-width: 320px) and (max-width: 768px){
+  display: flex;
+  flex-direction: column;
+
+  img{
+    width: 50vw;
+  }
+}
 `
 
 
-//------------
+//------------ teste na navegação
 
 export const Header = styled.header`
 /* background-image: url("https://i0.statig.com.br/bancodeimagens/2f/ym/i8/2fymi85z5vo5pcl5rsnsr3xgi.jpg");
@@ -52,4 +65,61 @@ background-repeat: no-repeat;
 width: 100%;
 height: 80vh; */
 
+`
+
+
+//Estilização Sobre
+export const MainSobre = styled.main`
+display: flex;
+justify-content: space-evenly;
+align-items:center;
+`
+
+export const SectionDoSobre = styled.section`
+height: 70vh;
+width: 30vw;
+margin-top: 3rem;
+border: solid red 5px;
+border-radius: 50px;
+h2{
+    text-align: center;
+}
+`
+
+export const Card = styled.section`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: space-evenly;
+height: 50vh;
+`
+
+export const DivCard = styled.div` 
+width: 25vw;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: space-evenly;
+height: 20vh; 
+h2{
+    color: tomato;
+}
+
+h3{
+    color: violet;
+}
+
+`
+
+export const DivCard2 = styled.div`
+width: 70%;
+display: flex;
+justify-content: space-evenly;
+align-items: center;
+flex-direction: column;
+height: 70vh;
+div{
+    display: flex;
+    align-items: center;
+}
 `
